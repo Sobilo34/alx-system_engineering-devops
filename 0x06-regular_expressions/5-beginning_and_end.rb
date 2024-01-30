4930ce0e515cdc7e5f8b9ca10ee3433221a15d4a
Sobilo34 <bilalsolih60@gmail.com>
2024-01-30 09:57:06 -0800

    regular expression must be exactly matching a string that starts with h ends with n and can have any single character in between

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# A regular expression that exactly matches a
+# string that starts with h ends with n and can
+# have any single character in between
+
+puts ARGV[0].scan(/h.n/).join
