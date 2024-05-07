9bfdc32e35d00fe55542492b4daea52c3fbe94a2
Sobilo34 <bilalsolih60@gmail.com>
2024-05-07 20:38:08 +0100

    calculate the subscribers in the data

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A function that queries the Reddit API and
+returns number id subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+
+    response = requests.get(url.format(subreddit), headers=headers)
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers', 0)
+    else:
+        return 0
