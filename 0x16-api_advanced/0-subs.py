0a5c1116fde1d672d207eaad541fc1cc68cffce7
Sobilo34 <bilalsolih60@gmail.com>
2024-05-07 20:48:27 +0100

    change the structure of the code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,18 +8,14 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Parameters:
-    subreddit (str): The subreddit to query.
-
-    Returns:
-        int: Number of subscribers for given subreddit
-        or 0 if subreddit is invalid.
+    function that calculate the subscribers
     """
     url = "https://www.reddit.com/r/{}/about.json"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 
     response = requests.get(url.format(subreddit), headers=headers)
-    if response.status_code == 200:
-        return response.json().get('data').get('subscribers', 0)
-    else:
+    if response.status_code != 200:
         return 0
+    else:
+        return response.json().get('data').get('subscribers', 0)
+
