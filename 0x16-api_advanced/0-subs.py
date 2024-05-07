c275a4791db296b172092e0ed5c1b3729b3a10d4
Sobilo34 <bilalsolih60@gmail.com>
2024-05-07 20:54:36 +0100

    set redirect to false

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 
-    response = requests.get(url.format(subreddit), headers=headers)
+    response = requests.get(url.format(subreddit), headers=headers. , allow_redirects=False)
     if response.status_code == 200:
         return response.json().get('data').get('subscribers', 0)
     else:
